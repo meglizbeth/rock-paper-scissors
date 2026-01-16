ac4ad7311f990004fccbc1443ac958d5bea1243f
meglizbeth <225731058+meglizbeth@users.noreply.github.com>
2026-01-16 14:15:15 -0500

    Write playRound() function
    Write playRound() function so it works and correct getComputerChoice() and getHumanChoice() functions

--- script.js
+++ script.js
@@ -1,26 +1,64 @@
-function computerAnswer() {
-        return Math.random();
-}
 
-function getComputerChoice() { 
-    if (computerAnswer() <0.33) {
-        console.log("rock");
+
+function getComputerChoice() {  
+    let computerChoice = Math.random();
+    
+    if (computerChoice <0.33) {
+        return "rock";
     }
-    else if (computerAnswer() <0.66) {
-        console.log("paper");
+    else if (computerChoice <0.66) {
+        return "paper";
     }
     else {
-        console.log("scissors");
+        return "scissors";
     }
-}
+};
+
+
 
 function getHumanChoice() {
-    let sign = prompt("Do you choose rock, paper, or scissors?");
-    if (sign === "rock") {
-        console.log("rock");
-    } else if (sign === "paper") {
-        console.log("paper");
-    } else if (sign === "scissors") {
-        console.log("scissors");
-    }
-}
+    let humanChoice = prompt("Do you choose rock, paper, or scissors?");   
+    
+    if (humanChoice.toLowerCase() === "rock") {
+        return "rock";
+        } 
+        else if (humanChoice.toLowerCase() === "paper") {
+            return "paper";
+        } 
+        else if (humanChoice.toLowerCase() === "scissors") {
+            return "scissors";
+        }
+    };
+
+let humanScore = 0;
+let computerScore = 0;
+
+function playRound() {
+    let humanSelection = getHumanChoice();
+    let computerSelection = getComputerChoice();
+
+    if (humanSelection === "rock" && computerSelection === "paper") {
+        return "Paper beats rock. You lose!";
+        } else if (humanSelection === "rock" && computerSelection === "scissors") {
+            return "Rock beats scissors. You win!";
+        } else if (humanSelection === "rock" && computerSelection === "rock") {
+            return "You both played rock. You tie!";
+        } else if (humanSelection === "paper" && computerSelection === "rock") {
+            return "Paper beats rock. You win!";
+        } else if(humanSelection === "paper" && computerSelection === "paper") {
+            return "You both played paper. You tie!";
+        } else if(humanSelection === "paper" && computerSelection === "scissors") {
+            return "Scissors beats paper. You lose!";
+        } else if(humanSelection  === "scissors" && computerSelection === "rock") {
+            return "Rock beats scissors. You lose!";
+        } else if(humanSelection  === "scissors" && computerSelection === "paper") {
+            return "Scissors beats paper. You win!";
+        } else if(humanSelection === "scissors" && computerSelection === "scissors") {
+            return "You both played scissors. You tie!";
+        }
+    };
+
+
+
+
+
