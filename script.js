a5614e048ebbf99d708c3e97316d657a2e81f794
meglizbeth <225731058+meglizbeth@users.noreply.github.com>
2026-01-21 13:37:34 -0500

    Rewrite functions
    Simplify playRound() function and deleted redundant functions

--- script.js
+++ script.js
@@ -26,57 +26,31 @@ function getHumanChoice() {
         }
     };
 
-let humanScore = 0;
-let computerScore = 0;
+
+const humanSelection = getHumanChoice();
+const computerSelection = getComputerChoice();
 
 function playRound() {
-    let humanSelection = getHumanChoice();
-    let computerSelection = getComputerChoice();
+    if (humanSelection === computerSelection) {
+        return "draw";
+    } else if 
+        (humanSelection === "rock" && computerSelection === "scissors" ||
+        humanSelection === "paper" && computerSelection === "rock" ||
+        humanSelection === "scissors" && computerSelection === "paper") {
+        return "humanWin";
+    } else {
+        return "computerWin";
+    }
+};    
 
-    if (humanSelection === "rock" && computerSelection === "paper") {
-        return "Paper beats rock. You lose!";
-        } else if (humanSelection === "rock" && computerSelection === "scissors") {
-            return "Rock beats scissors. You win!";
-        } else if (humanSelection === "rock" && computerSelection === "rock") {
-            return "You both played rock. You tie!";
-        } else if (humanSelection === "paper" && computerSelection === "rock") {
-            return "Paper beats rock. You win!";
-        } else if(humanSelection === "paper" && computerSelection === "paper") {
-            return "You both played paper. You tie!";
-        } else if(humanSelection === "paper" && computerSelection === "scissors") {
-            return "Scissors beats paper. You lose!";
-        } else if(humanSelection  === "scissors" && computerSelection === "rock") {
-            return "Rock beats scissors. You lose!";
-        } else if(humanSelection  === "scissors" && computerSelection === "paper") {
-            return "Scissors beats paper. You win!";
-        } else if(humanSelection === "scissors" && computerSelection === "scissors") {
-            return "You both played scissors. You tie!";
-        }
-    };
+let humanScore = 0;
+let computerScore = 0;
+let winner = playRound();
 
-function determineWinner() {
-    let result = playRound();
-    if (result === "Rock beats scissors. You win!" || 
-        "Paper beats rock. You win!" ||
-        "Scissors beats paper. You win!") {
-            return "humanWin";
-        } else if (result === "Paper beats rock. You lose!" ||
-        "Scissors beats paper. You lose!" ||
-        "Rock beats scissors. You lose!") {
-            return "computerWin"; 
-        } else if (result === "You both played rock. You tie!" ||
-        "You both played paper. You tie!" ||
-        "You both played scissors. You tie!") {
-            return "draw";
-        }
-    };
-
-function updateScore() {
-    let winner = determineWinner();
-    
+function updateScore() {    
     if (winner === "humanWin") {
-        return humanScore++;
+        return humanScore +1;
         } else if (winner === "computerWin") {
-            return computerScore++;
-        }
-    };
+            return computerScore +1;
+    }
+};
