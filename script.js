923b8e866e7c1ab837360eb2107582386d16151f
meglizbeth <225731058+meglizbeth@users.noreply.github.com>
2026-01-23 13:58:41 -0500

    Write playGame() function
    Write and test playGame() function and include endGame() function

--- script.js
+++ script.js
@@ -28,27 +28,16 @@ let humanScore = 0;
 let computerScore = 0;
 const maxScore = 5;
 
-function endGame() {
 
-    if (humanScore >= maxScore ||
-        computerScore >= maxScore) {
-        console.log("Game over! Your final score is " + humanScore
-            + "and the computer's final score is " + computerScore);
-        }
-    };
-
-function wait() {
-    2000;
-};
 
 function playRound() {
     const humanSelection = getHumanChoice();
     const computerSelection = getComputerChoice();
     
-    console.log("You chose " + humanSelection);
-    console.log("Computer chose " + computerSelection);
+    console.log("You chose " + humanSelection + ".");
+    console.log("Computer chose " + computerSelection + ".");
     if (humanSelection === computerSelection) {
-        console.log("Draw");
+        console.log("Draw.");
     } else if 
         (humanSelection === "rock" && computerSelection === "scissors" ||
         humanSelection === "paper" && computerSelection === "rock" ||
@@ -59,10 +48,32 @@ function playRound() {
         console.log("The computer won this round.")
         computerScore++;
     }
-    console.log("Your score is " + humanScore + ". The computer's score is " + computerScore + ".");
+        console.log("Your score is " + humanScore + ". The computer's score is " + computerScore + ".");
 };
 
-playRound();
-playRound();
+function endGame() {
+    console.log("Game over! Your final score is " + humanScore
+            + " and the computer's final score is " + computerScore + ".");
+    if (humanScore > computerScore) {
+        console.log("You won the game!");
+    }
+    else if (computerScore > humanScore) {
+        console.log("The computer won the game.");
+    }
+    else if (humanScore === computerScore) {
+        console.log("It's a tie!");
+    }
+};
+
+function playGame() {
+    playRound();
+    playRound();
+    playRound();
+    playRound();
+    playRound();
+    endGame();
+};
+
+playGame();
 
 
